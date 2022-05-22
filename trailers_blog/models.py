b5d9038d7ee2f5219a37d1ac52a2b42591403fde
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-22 20:12:15 +0000

    Add models to admin panel

--- trailers_blog/models.py
+++ trailers_blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
