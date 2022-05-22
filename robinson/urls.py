af17af2b30f9f5e00b95e72d611ffbe7d57604fc
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-22 19:54:46 +0000

    Add urls file and path to home page view

--- robinson/urls.py
+++ robinson/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('trailers_blog.urls')),
+    path('', include('trailers_blog.urls'), name='trailers_blog.urls'),
 ]
