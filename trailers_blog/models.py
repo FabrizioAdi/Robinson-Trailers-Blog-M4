6a80496336e2fd9af60413c65d6c8a62bd4fdc2b
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-23 19:24:49 +0000

    Add changes to title tag

--- trailers_blog/models.py
+++ trailers_blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=250)
-    title_tag = models.CharField(max_length=250, default="Robinson Trailers Blog")
+    title_tag = models.CharField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
