e520365600454f0f8ad3c906d07a035eb04ff77d
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-23 20:38:55 +0000

    Add placeholders to display the info message

--- trailers_blog/forms.py
+++ trailers_blog/forms.py
@@ -5,11 +5,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'title_tag', 'author', 'featured_image', 'excerpt', 'body')
-
+# Python dictionary add many items you like
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add your title'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add your tag here'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
-            'excerpt': forms.Textarea(attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'excerpt': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Add your content'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Add your content'}),
         }
