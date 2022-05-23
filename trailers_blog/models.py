963243f9ca7d569a157ef51a0b1ac8ef826f12d8
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-23 18:15:46 +0000

    Add title tag to models

--- trailers_blog/models.py
+++ trailers_blog/models.py
@@ -5,6 +5,7 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=250)
+    title_tag = models.CharField(max_length=250, default="Robinson Trailers Blog")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
