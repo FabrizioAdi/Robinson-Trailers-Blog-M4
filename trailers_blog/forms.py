3601920d84a61fc42cafad0dbc21a20c081a05bc
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-24 15:41:20 +0000

    Add Edit Form class

--- trailers_blog/forms.py
+++ trailers_blog/forms.py
@@ -13,3 +13,16 @@ class PostForm(forms.ModelForm):
             'excerpt': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Add your content'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Add your content'}),
         }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'featured_image', 'excerpt', 'body')
+# Python dictionary add many items you like
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add your title'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add your tag here'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
+            'excerpt': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Add your content'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Add your content'}),
+        }
