c61b3f531cedea3009be67d0023bcbfbf06c853b
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-24 16:12:26 +0000

    Add success url to redirect to home page after delete blog post

--- trailers_blog/views.py
+++ trailers_blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 # def home(request):
@@ -32,3 +33,4 @@ class EditPostView(UpdateView):
 class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
