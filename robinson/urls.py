14e70dab7068cb9e3ff77c3cee80dbec4611ef67
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-25 15:49:12 +0000

    Add path to allauth.urls

--- robinson/urls.py
+++ robinson/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('trailers_blog.urls'), name='trailers_blog.urls'),
+    path('accounts/', include('allauth.urls')),
 ]
