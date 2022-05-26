a4620daa99224a3355aa5230039ea23b39b0d756
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-26 14:53:43 +0000

    Add new Category model, and register in admin site

--- trailers_blog/models.py
+++ trailers_blog/models.py
@@ -3,6 +3,16 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.urls import reverse
 
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        #return reverse('home')
+        return reverse('post-detail', args=(str(self.id)))
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=250)
@@ -13,6 +23,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=250, default='forestry')
 
     def __str__(self):
         return self.title
