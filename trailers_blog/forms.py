65d4292628b987b067fe3feb31a5f6d643451c9c
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-26 15:27:38 +0000

    Add category forms select, and create a query for category, to display diffrent category by drop manu

--- trailers_blog/forms.py
+++ trailers_blog/forms.py
@@ -1,16 +1,19 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+choices = Category.objects.all().values_list('name', 'name')
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'featured_image', 'excerpt', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'featured_image', 'excerpt', 'body')
 # Python dictionary add many items you like
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add your title'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add your tag here'}),
-            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'user', 'type':'hidden'}),
-            #'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            #'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'user', 'type':'hidden'}),
+            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
             'excerpt': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Add your content'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Add your content'}),
         }
