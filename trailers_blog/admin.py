5495aca92ac58ab00233f4e5b7a0315dd01c6d33
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-30 17:55:54 +0000

    Add admin register post decorator

--- trailers_blog/admin.py
+++ trailers_blog/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Post, Category
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Post)
+# admin.site.register(Post)
 admin.site.register(Category)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
